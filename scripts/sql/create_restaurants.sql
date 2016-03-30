35472d3991f476eae1ac409719accfa2dafdfc66
dinesh_panchananam <dineshpanchananam@gmail.com>
2016-03-29 21:47:23 -0400

    project structure changes

--- scripts/sql/create_restaurants.sql
+++
@@ -1,4 +0,0 @@
-create table restaurant (
-  name varchar(20),
-  address varchar(30),
-  zipcode int);
--- scripts/sql/create_users.sql
+++
@@ -1,6 +0,0 @@
-create table customers (
-  email varchar(20),
-  name varchar(20),
-  password varchar(20),
-  address varchar(30),
-  zipcode int);
