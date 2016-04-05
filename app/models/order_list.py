14eba3e89973b32fdbf15cac9e96acba3e1294cf
lgopalab <mah!dhar2315>
2016-04-05 05:09:16 -0400

    Order Model changed and Login page changed (Only HTML file not the handling part)

--- app/models/order_list.py
+++ app/models/order_list.py
@@ -4,9 +4,11 @@ from app.util.database import db
 class Order_List(db.Model):
     _id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.Integer)
+    delivery_id = db.Column(db.Integer)
 
-    def __init__(self, user_id):
+    def __init__(self, customer_id, delivery_id):
         self.customer_id = customer_id
+        self.delivery_id = delivery_id
 
     def __repr__(self):
-        return "User ID:%s " % (self.customer_id)
+        return "User ID:%s Delivered by %s " % (self.customer_id, self.delivery_id)
