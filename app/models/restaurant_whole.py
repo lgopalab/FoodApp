f95e5f3146b35dc25ba57b982dbee94132fe8d4f
lgopalab <mah!dhar2315>
2016-04-12 00:55:50 -0400

    Added modified restaurants model (Includes Restaurants and Owner's data as well)

---
+++ app/models/restaurant_whole.py
@@ -0,0 +1,24 @@
+from app.util.database import db
+
+
+class Restaurant_whole(db.Model):
+    _id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(30))
+    rest_name = db.Column(db.String(20))
+    owner_name = db.Column(db.String(20))
+    password = db.Column(db.String(20))
+    address = db.Column(db.String(30))
+    zipcode = db.Column(db.Integer)
+    rating = db.Column(db.Integer)
+
+    def __init__(self, email, rest_name, owner_name, password, address, zipcode, rating):
+        self.email = email
+        self.rest_name = rest_name
+        self.owner_name = owner_name
+        self.password = password
+        self.address = address
+        self.zipcode = zipcode
+        self.rating = rating
+
+    def __repr__(self):
+        return "name:%s, mail:%s" % (self.name, self.email)
